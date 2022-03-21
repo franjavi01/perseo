6a544447489983773af2afbba1bb328691f0c6cf
franjavi01 <fjsg0001@gmail.com>
2022-03-21 10:00:59 +0100

    estructura básica del proyecto

---
+++ index.php
@@ -0,0 +1,8 @@
+    <?php require_once 'header.php';  ?>
+    <main>
+        <h1>Bienvenido a Perseo Cursos Online</h1>
+        <h2>Hola, soy Patricio, director general de Perseo. Si quieres promocionar tu carrera profesinal estas en el sitio adecuado</h2>
+        <h2>Regístrate e incríbete en el curso que más se adecue a tu perfil profesional</h2>
+    </main>
+    <?php require_once 'footer.php';  ?>
+    
