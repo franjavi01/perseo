2bbf5475d5b6538aa6b7a502893196db094acb9b
franjavi01 <fjsg0001@gmail.com>
2022-04-29 16:00:05 +0200

    añadido formulario de creación de cursos del admin

--- admin.php
+++ admin.php
@@ -1,7 +1,23 @@
 <?php include_once "headerHome.php"; ?>
 
 <main>
-<p>Estas en la vista de administrador</p>
+    <h2>Introduce el nombre del curso</h2>
+    <form action="./adminCrud.php" method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="curso" class="form-label">Nombre del curso</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="imagen" class="form-label">Imagen:</label>
+            <input type="file" class="form-control">
+            <div class="form-text">Sube una imagen en formato JPG o PNG</div>
+        </div>
+        <div class="mb-3">
+            <label for="precio" class="form-label">Precio:</label>
+            <input type="text" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-warning">Enviar</button>
+    </form>
 </main>
 
 <?php require_once 'footer.php';  ?>
